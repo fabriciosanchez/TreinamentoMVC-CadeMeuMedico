f174a40f2c23c97c17ffe2b489c2126695150bcb
Fabrício Sanchez <fabricio.sanchez@gmail.com>
2015-11-12 10:06:31 -0200

    Commit inicial

---
+++ Exemplo2-CadeMeuMedico/Controllers/PrincipalController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Exemplo2_CadeMeuMedico.Controllers
+{
+    public class PrincipalController : Controller
+    {
+        // GET: Principal
+        public ActionResult Index()
+        {
+            int Valor;
+
+            if (RouteData.Values["id"] != null)
+            {
+                Valor = Convert.ToInt16(RouteData.Values["id"].ToString());
+                if (Valor == 1)
+                {
+                    ViewBag.Mensagem = "Bom dia!";
+                }
+                else
+                {
+                    ViewBag.Mensagem = "Boa noite";
+                } 
+            }
+            else
+            {
+                ViewBag.Mensagem = "Nenhum parâmetro informado.";
+            }
+
+            return View();
+        }
+
+        public ActionResult pesquisa()
+        {
+            string PrimeiraParte;
+
+            if (RouteData.Values["fabricio"] != null)
+            {
+                PrimeiraParte = RouteData.Values["fabricio"].ToString();
+
+                if (PrimeiraParte != "fabricio")
+                {
+                    ViewBag.Mensagem = "Rota incorreta";
+                }
+                else
+                {
+                    ViewBag.Mensagem = "Rota validada com sucesso";
+                } 
+            }
+
+            return View();
+        }
+    }
+}
